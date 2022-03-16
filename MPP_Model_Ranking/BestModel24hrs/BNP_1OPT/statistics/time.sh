f88f94f956ebc2e38b9faffa2d687080a6c3f454
chaconcastillojoel <joel.chacon@cimat.mx>
2022-03-16 12:43:45 -0600

    resuls to now

--- MPP_Model_Ranking/BestModel24hrs/BNP_1OPT/statistics/time.sh
+++ MPP_Model_Ranking/BestModel24hrs/BNP_1OPT/statistics/time.sh
@@ -1,13 +1,17 @@
 for nday in 20 40 60;
 do
  echo $nday
+  for w in 10 20 30 40 50 60 70 80 90 100;
+  do
      rm $nday
      for i in ../History/*days_${nday}_*;
      do
-      tail -10 $i | awk '{print $NF}'| sort -n | head -n 1 >> $nday
+       
+       head -n $w $i | tail -10 | awk '{print $NF}'| sort -n | head -n 1 >> $nday
      done
      echo "Dias.. $nday"
    Rscript -e 'd<-scan("stdin", quiet=TRUE)' \
    	-e 'sprintf(summary(d), fmt="%.3f")' < $nday
    	#-e 'print(summary(d), digits=20)' < $nday
+   done
 done
